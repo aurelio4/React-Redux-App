0009cde6d91571ad7c274a662eb1981641613516
chelkuhs <aurelioarcabascio@gmail.com>
2020-04-15 21:19:40 -0400

    changed some stuff

--- react-redux-app/src/components/ErrorDisplay.js
+++ react-redux-app/src/components/ErrorDisplay.js
@@ -3,7 +3,29 @@ import '../App.css'
 
 function ErrorDisplay(props) {
   return(
-    <h1 id="error-text">Error: {props.error}</h1>
+    <>
+      <div>
+        {/* N */}
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/201-n.png" alt="N" />
+        {/* O */}
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/201-o.png" alt="O" />
+        {/* T */}
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/201-t.png" alt="T" />
+      </div>
+
+      <div id="found">
+        {/* F */}
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/201-f.png" alt="F" />
+        {/* O */}
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/201-o.png" alt="O" />
+        {/* U */}
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/201-u.png" alt="U" />
+        {/* N */}
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/201-n.png" alt="N" />
+        {/* D */}
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/201-d.png" alt="D" />
+      </div>
+    </>
   )
 }
 
